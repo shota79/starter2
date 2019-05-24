f9e0fe1cc4a2fbc13ecabba1b9cd36a21e896c06
UILab_mac1 <junheelee@UILab-mac1ui-MacBook.local>
2019-05-25 02:14:51 +0900

    1차 수정

--- gulpfile.js
+++ gulpfile.js
@@ -11,30 +11,39 @@ const gulp = require('gulp'),
       del = require('del');
 
 const src = 'src';
+const dist = 'dist';
 const paths = {
-  styles: {
-    src: src + '/scss/**/**/*.scss',
-    dest: 'assets/css'
+  scss: {
+    src: src + '/assets/scss/**/**/*.scss',
+    dest: dist + '/assets/css'
+  },
+  css: {
+    src: src + '/assets/css/**/**/*.css',
+    dest: dist + '/assets/css'
   },
   scripts: {
-    src: src + '/js/**/*.js',
-    dest: 'assets/js'
+    src: src + '/assets/js/**/*.js',
+    dest: dist + '/assets/js'
   },
   components: {
-    src: src + '/components/*.html',
-    dest: 'assets/components'
+    src: src + '/components/**/*.html',
+    dest: dist + '/components'
   },
   fonts: {
-    src: src + '/fonts/**/*',
-    dest: 'assets/fonts'
+    src: src + '/assets/fonts/**/*',
+    dest: dist + '/assets/fonts'
+  },
+  pages: {
+    src: src + '/pages/**/*.html',
+    dest: dist + '/pages'
   },
   index: {
     src: src + '/*.html',
-    dest: 'assets'
+    dest: dist
   },
   images: {
-    src: src + '/images/**/*.{jpg, jpeg, png, gif}',
-    dest: 'assets/images/'
+    src: src + '/assets/img/**/*.{jpg, jpeg, png, gif}',
+    dest: dist + '/assets/img/'
   }
 }
 
@@ -43,7 +52,7 @@ const browserSync = done => {
     port: 3000,
     open: false,
     server: {
-      baseDir: './assets'
+      baseDir: './dist'
     }
   })
 }
@@ -58,8 +67,8 @@ const browserSyncReload = path => {
   return gulp.src(path).pipe(bs.reload({stream: true}));
 }
 
-const styles = () => {
-  return gulp.src(paths.styles.src, { sourcemaps: true })
+const scss = () => {
+  return gulp.src(paths.scss.src, { sourcemaps: true })
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(cleanCSS({
@@ -67,7 +76,16 @@ const styles = () => {
     }))
     .pipe(sourcemaps.write('../maps'))
     .pipe(cache.clear())
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(gulp.dest(paths.scss.dest))
+}
+
+const css = () => {
+  return gulp.src([paths.css.src])
+    .pipe(cleanCSS({
+      debug: true
+    }))
+    .pipe(cache.clear())
+    .pipe(gulp.dest(paths.css.dest))
 }
 
 const scripts = () => {
@@ -83,26 +101,51 @@ const components = () => {
     .pipe(cache.clear())
 }
 
+const copyPages = () => {
+  return gulp.src(paths.pages.src)
+    .pipe(htmlPartial({
+      basePath: src + '/components/',
+      tagName: 'component',
+      variablePrefix: '@@'
+    }))
+    .pipe(gulp.dest(paths.pages.dest))
+    .pipe(cache.clear())
+}
+
 const copyIndex = () => {
   return gulp.src(paths.index.src)
     .pipe(htmlPartial({
-      basePath: 'src/components/'
+      basePath: 'src/components/',
+      tagName: 'component',
+      variablePrefix: '@@'
     }))
     .pipe(gulp.dest(paths.index.dest))
     .pipe(cache.clear())
 }
 const fonts = () => gulp.src(paths.fonts.src).pipe(gulp.dest(paths.fonts.dest))
-const clean = () => del(['./assets']);
+const clean = () => del(['./dist']);
 
 const watch = () => {
-  gulp.watch(paths.styles.src, styles).on('change', path => browserSyncStream(path));
+  gulp.watch(paths.css.src, css).on('change', path => browserSyncStream(path));
+  gulp.watch(paths.scss.src, scss).on('change', path => browserSyncStream(path));
   gulp.watch(paths.scripts.src, scripts).on('change', path => browserSyncReload(path));
   gulp.watch(paths.components.src, components).on('change', path => browserSyncReload(path));
+  gulp.watch(paths.pages.src, copyPages).on('change', path => browserSyncReload(path));
   gulp.watch(paths.index.src, copyIndex).on('change', path => browserSyncReload(path));
+  
 }
 
-const serve = gulp.series(clean, gulp.parallel( browserSync, styles, scripts, watch, fonts, copyIndex));
-const build = gulp.series(clean, gulp.parallel( styles, scripts, copyIndex, fonts ));
+// css only
+const serve = gulp.series(clean, gulp.parallel( browserSync, css, scripts, watch, fonts, copyPages, copyIndex));
+const build = gulp.series(clean, gulp.parallel( css, scripts, copyPages, copyIndex, fonts ));
+
+// scss only
+// const serve = gulp.series(clean, gulp.parallel( browserSync, scss, scripts, watch, fonts, copyPages, copyIndex));
+// const build = gulp.series(clean, gulp.parallel( scss, scripts, copyPages, copyIndex, fonts ));
+
+// css & scss
+// const serve = gulp.series(clean, gulp.parallel( browserSync, css, scss, scripts, watch, fonts, copyPages, copyIndex));
+// const build = gulp.series(clean, gulp.parallel( css, scss, scripts, copyPages, copyIndex, fonts ));
 
 gulp.task('default', serve);
 gulp.task('build', build);
